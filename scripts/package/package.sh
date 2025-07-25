5f339241b51966760470d0534ebaa7368b8b8d82
PancakeTAS <pancake@mgnet.work>
2025-07-26 00:10:05 +0200

    workflows: properly include resources

--- scripts/package/package.sh
+++ scripts/package/package.sh
@@ -22,9 +22,12 @@ envsubst < scripts/package/alpm.PKGINFO > alpm/.PKGINFO
 mkdir -pv alpm/usr/{bin,lib,share/vulkan/implicit_layer.d,share/applications,share/icons/hicolor/256x256/apps}
 cp -v bin/lsfg-vk-ui alpm/usr/bin/lsfg-vk-ui
 cp -v lib/liblsfg-vk.so alpm/usr/lib/liblsfg-vk.so
-cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json alpm/usr/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.jsonc
-cp -v ui/rsc/gay.pancake.lsfg-vk-ui.desktop alpm/usr/share/applications/lsfg-vk-ui.desktop
-cp -v ui/rsc/icon.png alpm/usr/share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png
+cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json \
+    alpm/usr/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.jsonc
+cp -v share/applications/gay.pancake.lsfg-vk-ui.desktop \
+    alpm/usr/share/applications/lsfg-vk-ui.desktop
+cp -v share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png \
+    alpm/usr/share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png
 
 tar -cvzf "lsfg-vk-$VERSION.x86_64.tar.zst" -C alpm \
     .PKGINFO usr
@@ -38,9 +41,12 @@ envsubst < scripts/package/dpkg.control > deb/DEBIAN/control
 mkdir -pv deb/usr/{bin,lib,share/vulkan/implicit_layer.d,share/applications,share/icons/hicolor/256x256/apps}
 cp -v bin/lsfg-vk-ui deb/usr/bin/lsfg-vk-ui
 cp -v lib/liblsfg-vk.so deb/usr/lib/liblsfg-vk.so
-cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json deb/usr/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json
-cp -v ui/rsc/gay.pancake.lsfg-vk-ui.desktop deb/usr/share/applications/lsfg-vk-ui.desktop
-cp -v ui/rsc/icon.png deb/usr/share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png
+cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json \
+    deb/usr/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json
+cp -v share/applications/gay.pancake.lsfg-vk-ui.desktop \
+    deb/usr/share/applications/lsfg-vk-ui.desktop
+cp -v share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png \
+    deb/usr/share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png
 
 dpkg-deb --root-owner-group --build deb "lsfg-vk-$VERSION.x86_64.deb"
 
@@ -53,9 +59,12 @@ envsubst < scripts/package/rpm.spec > rpm/lsfg-vk.spec
 mkdir -pv rpm/SOURCES
 cp -v bin/lsfg-vk-ui rpm/SOURCES
 cp -v lib/liblsfg-vk.so rpm/SOURCES
-cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json rpm/SOURCES
-cp -v ui/rsc/gay.pancake.lsfg-vk-ui.desktop rpm/SOURCES/lsfg-vk-ui.desktop
-cp -v ui/rsc/icon.png rpm/SOURCES/gay.pancake.lsfg-vk-ui.png
+cp -v share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json \
+    rpm/SOURCES
+cp -v share/applications/gay.pancake.lsfg-vk-ui.desktop \
+    rpm/SOURCES/lsfg-vk-ui.desktop
+cp -v share/icons/hicolor/256x256/apps/gay.pancake.lsfg-vk-ui.png \
+    rpm/SOURCES/gay.pancake.lsfg-vk-ui.png
 
 rpmbuild -bb rpm/lsfg-vk.spec --define "_topdir $(pwd)/rpm"
 mv -v "rpm/RPMS/x86_64/lsfg-vk-$VERSION-1.x86_64.rpm" "lsfg-vk-$VERSION.x86_64.rpm"
