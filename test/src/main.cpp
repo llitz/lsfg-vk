8602f45b3fc485205edab4df71103e77fd930145
PancakeTAS <pancake@mgnet.work>
2025-08-08 17:37:14 +0200

    feat(test): write main test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,13 +1,16 @@
+#include "common/utils.hpp"
+#include "core/commandpool.hpp"
 #include "core/device.hpp"
+#include "core/image.hpp"
 #include "core/instance.hpp"
 #include "extract/extract.hpp"
-#include "mini/image.hpp"
-#include "utils.hpp"
 
 #include <vulkan/vulkan_core.h>
 #include <renderdoc_app.h>
+#include <unistd.h>
 #include <dlfcn.h>
 
+#include <utility>
 #include <cstddef>
 #include <cstdint>
 #include <string>
@@ -20,6 +23,7 @@ using namespace LSFG;
 
 const VkExtent2D SRC_EXTENT = { 2560 , 1440 };
 const VkFormat SRC_FORMAT = VK_FORMAT_R8G8B8A8_UNORM;
+const std::string EMPTY_FILE = "test/empty.dds";
 const std::array<std::string, 2> SRC_FILES = {
     "test/f0.dds",
     "test/f1.dds"
@@ -53,26 +57,28 @@ namespace {
     }
 
     /// Create images for frame generation
-    void create_images(const Core::Device& device,
+    std::pair<Core::Image, Core::Image> create_images(const Core::Device& device,
             std::array<int, 2>& fds,
-            std::vector<int>& outFds, std::vector<Mini::Image>& out_n) {
-        const Mini::Image frame_0{device.handle(), device.getPhysicalDevice(),
+            std::vector<int>& outFds, std::vector<Core::Image>& out_n) {
+        const Core::Image frame_0{device,
             SRC_EXTENT, SRC_FORMAT,
             VK_IMAGE_USAGE_TRANSFER_DST_BIT, VK_IMAGE_ASPECT_COLOR_BIT,
             &fds.at(0)
         };
-        const Mini::Image frame_1{device.handle(), device.getPhysicalDevice(),
+        const Core::Image frame_1{device,
             SRC_EXTENT, SRC_FORMAT,
             VK_IMAGE_USAGE_TRANSFER_DST_BIT, VK_IMAGE_ASPECT_COLOR_BIT,
             &fds.at(1)
         };
 
         for (size_t i = 0; i < (MULTIPLIER - 1); i++)
-            out_n.at(i) = Mini::Image{device.handle(), device.getPhysicalDevice(),
+            out_n.at(i) = Core::Image{device,
                 SRC_EXTENT, SRC_FORMAT,
                 VK_IMAGE_USAGE_TRANSFER_SRC_BIT, VK_IMAGE_ASPECT_COLOR_BIT,
                 &outFds.at(i)
             };
+
+        return { frame_0, frame_1 };
     }
 
     /// Create the LSFG context.
@@ -99,7 +105,8 @@ namespace {
 namespace {
     std::array<int, 2> fds{};
     std::vector<int> outFds(MULTIPLIER - 1);
-    std::vector<Mini::Image> out_n(MULTIPLIER - 1);
+    std::pair<Core::Image, Core::Image> frames{};
+    std::vector<Core::Image> out_n(MULTIPLIER - 1);
     int32_t lsfg_id{};
 }
 
@@ -107,14 +114,33 @@ int main() {
     // initialize host Vulkan
     const Core::Instance instance{};
     const Core::Device device{instance, 0x1463ABAC};
+    const Core::CommandPool commandPool{device};
 
     // setup test
     setup_renderdoc();
-    create_images(device, fds, outFds, out_n);
+    frames = create_images(device, fds, outFds, out_n);
     lsfg_id = create_lsfg(fds, outFds);
+
+    Utils::clearImage(device, frames.first);
+    Utils::clearImage(device, frames.second);
 
     // run
     for (size_t fc = 0; fc < SRC_FILES.size(); fc++) {
+        if (fc % 2 == 0)
+            Utils::uploadImage(device, commandPool, frames.first, SRC_FILES.at(fc));
+        else
+            Utils::uploadImage(device, commandPool, frames.second, SRC_FILES.at(fc));
+
+        if (rdoc) rdoc->StartFrameCapture(nullptr, nullptr);
+
+        // run the present
+        const std::vector<int> null_sems(MULTIPLIER - 1, -1);
+        presentContext(lsfg_id, -1, null_sems);
+
+        // wait until the present is done
+        usleep(1000 * 100);
+
+        if (rdoc) rdoc->EndFrameCapture(nullptr, nullptr);
     }
 
     // destroy test
