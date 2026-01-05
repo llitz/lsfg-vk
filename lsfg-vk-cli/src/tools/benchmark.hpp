8203e32d99e198b38da6a6c51dbf8aaa9d430a48
PancakeTAS <pancake@mgnet.work>
2026-01-05 22:11:45 +0100

    fix: cli: do not set irreversible benchmark defaults

--- lsfg-vk-cli/src/tools/benchmark.hpp
+++ lsfg-vk-cli/src/tools/benchmark.hpp
@@ -10,13 +10,13 @@ namespace lsfgvk::cli::benchmark {
     /// options for the "benchmark" command
     struct Options {
         std::optional<std::string> dll;
-        bool allow_fp16{true};
+        bool allow_fp16{false};
         int width{1920};
         int height{1080};
 
-        float flow{0.85F};
+        float flow{1.0F};
         int multiplier{2};
-        bool performance_mode{true};
+        bool performance_mode{false};
         std::optional<std::string> gpu;
 
         int duration{10};
